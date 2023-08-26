9f9fc0ac9b1c52ae100b7972d81cbeef123daf12
Christian Valeri <mortlaxris@outlook.com>
2023-08-25 20:15:16 -0500

    Mapped the fetched data into divs

--- src/products.jsx
+++ src/products.jsx
@@ -1,13 +1,32 @@
 import { useState } from 'react'
+import { useEffect } from 'react';
 import './Products.css'
 import NavBar from './NavBar.jsx'
 
 function Products() {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products?limit=4')
+            .then(response => response.json())
+            .then((data) => setData(data))
+            .catch((error) => console.error(error));
+    }, []);
+
     return (
         <>
             <NavBar />
+            <div>
+                {data.map(product => (
+                    <div key={product.id}>
+                        <img src={product.image} alt={product.title} />
+                        <h1>{product.title}</h1>
+                        <p>{product.description}</p>
+                        <p>{product.price}</p>
+                    </div>
+                ))}
+            </div>
         </>
-    )
+    );
 }
 
 export default Products;
