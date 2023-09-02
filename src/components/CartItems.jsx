7f60a74b7f1cbdbd45ebc076f49e317de16500dc
Christian Valeri <mortlaxris@outlook.com>
2023-09-02 16:20:49 -0500

    Added total Price div, added checkout btn

--- src/components/CartItems.jsx
+++ src/components/CartItems.jsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
 
 function CartItems({ products }) {
+    const [totalPrice, setTotalPrice] = useState(0);
+
     function QuantityButton() {
         const [quantity, setQuantity] = useState(0);
 
         function add() {
             setQuantity(quantity + 1);
         }
-    
         function subtract() {
             if (quantity > 0) {
-            setQuantity(quantity - 1);
+                setQuantity(quantity - 1);
             }
         }
         return (
@@ -27,16 +28,16 @@ function CartItems({ products }) {
     if (products && products.length === 0) {
         console.log("Empty Cart");
         return (
-        <>
-            <h2 id="cartEmtpy">Cart is currently Empty!</h2>
-        </>
+            <>
+                <h2 id="cartEmtpy">Cart is currently Empty!</h2>
+            </>
         )
     } else {
         console.log("Cart has items");
         return (
-        <div id="cartsContainer">
-            {products &&
-            products.map((product) => (
+            <div id="cartsContainer">
+                <h1 id="totalPrice">Total Price: {totalPrice}</h1>
+                {products && products.map((product) => (
                 <div className="cartBoxes" key={product.id}>
                     <img src={product.image} alt={product.title} />
                     <div className="cartProductInfoContainer">
@@ -45,9 +46,9 @@ function CartItems({ products }) {
                         <h2>${product.price}</h2>
                         <QuantityButton />
                     </div>
-
                 </div>
-            ))}
+                ))}
+                <button id="checkOutButton">CheckOut</button>
         </div>
         )
     }
